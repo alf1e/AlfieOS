bc10c44d6307fc3c7face3dd8bb8b3341f6cf2d4
Alfie4982 <alfiewbarlow@gmail.com>
2021-07-10 19:10:05 +0100

    Add project files.

---
+++ AlfieOS/Kernel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+using Cosmos.System.Graphics;
+using System.Drawing;
+
+namespace AlfieOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.Clear();
+            Console.WriteLine("");
+        }
+
+        protected override void Run()
+        {
+            string input = "";
+            input = Console.ReadLine();
+            Terminal(input);
+        }
+        private void Terminal(string input)
+        {
+            if (input == "help")
+            {
+                Console.WriteLine("help -- Shows help infomation");
+                Console.WriteLine("restart -- Reboot the machine");
+                Console.WriteLine("shutdown -- Shutdown the machine");
+            }
+            else if (input == "restart")
+            {
+                Sys.Power.Reboot();
+            }
+            else if (input == "shutdown")
+            {
+                Sys.Power.Shutdown();
+            }
+            
+        }
+    }
+}
---
+++ AlfieOS/Terminal1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+
+namespace AlfieOS
+{
+    internal class Terminal 
+    {
+        private string input;
+
+        public Terminal(string input)
+        {
+            if (input == "help")
+            {
+                Console.WriteLine("help -- Shows help infomation");
+                Console.WriteLine("restart -- Reboot the machine");
+                Console.WriteLine("shutdown -- Shutdown the machine");
+            }
+            else if (input == "restart")
+            {
+                Sys.Power.Reboot();
+            }
+            else if (input == "shutdown")
+            {
+                Sys.Power.Shutdown();
+            }
+        }
+    }
+}
